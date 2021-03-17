90cadc1ce495d89161d47b878be9527d64d540f9
Christian Balderrama <0christianbalderrama@gmail.com>
2021-03-18 00:21:02 +0800

    :sparkles: Add nodemon and fix babel issues

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import "regenerator-runtime";
+
 import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", async(_, res) => {
-  return res.send("Hello, world!");
+  return res.send("Hello, world! dd");
 });
 
-app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is listening to port ${PORT}`));
